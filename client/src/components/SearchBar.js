c7e7d3c03287ffd70dd7b6b186163b92d9671224
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-09-25 17:50:48 -0600

    g

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,43 +1,12 @@
-import React, { useState, useEffect } from "react";
-import '../App.css';
+import React from "react";
+import '../App.css'
 
-function SearchBar() {
-  const [searchQuery, setSearchQuery] = useState("");
-  const [pitchers, setPitchers] = useState([]);
-
-  useEffect(() => {
-    fetch('http://127.0.0.1:5555/pitchers')
-      .then(response => response.json())
-      .then(data => setPitchers(data))
-      .catch(error => console.error('Error fetching pitchers:', error));
-  }, []);
-
-  const handleSearch = (event) => {
-    setSearchQuery(event.target.value);
-  };
-
-  const filterPitchers = () => {
-    return pitchers.filter(pitcher =>
-      pitcher.name.toLowerCase().includes(searchQuery.toLowerCase())
-    );
-  };
-
-  return (
+function SearchBar(){
+return(
     <div>
-      <input
-        className="search-bar"
-        type="search"
-        placeholder="Find a pitcher"
-        value={searchQuery}
-        onChange={handleSearch}
-      />
-      <ul>
-        {filterPitchers().map(pitcher => (
-          <li key={pitcher.id}>{pitcher.name}</li>
-        ))}
-      </ul>
+        <input className="search-bar" type="search" placeholder="find a pitcher"></input>
     </div>
-  );
+)
 }
 
 export default SearchBar;
