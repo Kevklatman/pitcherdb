1e9ef09012bf2e25ee73d96022866ded75a05e4a
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-09-25 20:18:01 -0600

    kh rout

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import Home from './Home'; // Adjust the path as needed
 import NavBar from './NavBar';
+import Pitchers from './Pitchers';
 
 function App() {
+  const renderContent = () => {
+    const path = window.location.pathname;
+
+    switch (path) {
+      case '/':
+        return <Home />;
+      case '/pitchers':
+        return <Pitchers />;
+      case '/stats':
+      default:
+        return <Home />;
+    }
+  };
+
   return (
     <div>
-      <NavBar/>
-      <Home />
+      {<NavBar/>}
+      {renderContent()}
     </div>
   );
 }
+
 export default App;
---
+++ client/src/components/Pitchers.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Pitchers(){
+    return(
+        <div>
+            hello
+        </div>
+    )
+}
+
+export default Pitchers;
