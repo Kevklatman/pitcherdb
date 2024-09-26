15e253a0eb9c271abf3b827c57c853db7c29cf17
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-09-25 20:46:51 -0600

    button

--- client/src/components/PlayerCard.js
+++ client/src/components/PlayerCard.js
@@ -2,13 +2,13 @@ import React from 'react';
 import '../App.css';
 
 function PlayerCard({ pitcher }) {
-  return (
-    <div className='player-card'>
+  return (   
+    <button className='player-card'>
       <h3>{pitcher.name}</h3>
       <p>ERA: {pitcher.era}</p>
       <p>{pitcher.From} to {pitcher.To}</p>
       {/* Add more pitcher details here */}
-    </div>
+    </button>
   );
 }
 
